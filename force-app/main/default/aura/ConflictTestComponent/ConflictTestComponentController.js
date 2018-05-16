ac1c588f418d1ea4afc654256bea204111c0e9b0
TimoAarnio <timo.aarnio@ceili.fi>
2018-05-16 10:58:01 +0300

    Conflict test material lisätty

---
+++ force-app/main/default/aura/ConflictTestComponent/ConflictTestComponentController.js
@@ -0,0 +1,11 @@
+({
+    doInit : function(component, event, helper) {
+        // Ja tähän funktioon tehdään jotain ihan random-muutoksia olemassa oleviin riveihin
+        var action = component.get("c.getCurrentUser");
+        action.setCallback(this, function(response) {
+            var user = response.getReturnValue();
+            component.set("v.greeting", user.FirstName);
+        })
+        $A.enqueueAction(action);
+    }
+})
